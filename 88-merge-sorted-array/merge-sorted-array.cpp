2069031404fce68d08760fb6cadb1b1c5dd04f29
CHINMAY DONGRE <chinmaydongre24@gmail.com>
2026-08-17 23:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (13.92%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -2,30 +2,44 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int i = m - 1;       
-        int j = n - 1;        
-        int k = m + n - 1;    
-        while (i >= 0 && j >= 0)
-        {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        vector<int> temp(m + n);
+
+        // Compare both arrays
+        while (i < m && j < n) {
             
-            if (nums1[i] > nums2[j])
-            {
-                nums1[k] = nums1[i];
-                i--;
+            if (nums1[i] < nums2[j]) {
+                temp[k] = nums1[i];
+                i++;
             }
-            else
-            {
-                nums1[k] = nums2[j];
-                j--;
+            else {
+                temp[k] = nums2[j];
+                j++;
             }
 
-            k--;
+            k++;
         }
-        while (j >= 0)
-        {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
+
+        // Copy remaining elements of nums1
+        while (i < m) {
+            temp[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of nums2
+        while (j < n) {
+            temp[k] = nums2[j];
+            j++;
+            k++;
+        }
+
+        // Copy temp back to nums1
+        for (int x = 0; x < m + n; x++) {
+            nums1[x] = temp[x];
         }
     }
 };
